1f1cf2eb59bb3a276a3c35b3633bb4f893ee00d8
Gordon <gordonmes@gmail.com>
2021-03-05 01:24:12 +0100

    first methods

---
+++ game.py
@@ -0,0 +1,7 @@
+class Game:
+    def __init__(self, players):
+        players = {player: 0 for player in players}
+        return
+
+    def check_win(self):
+        return
--- main.py
+++ main.py
@@ -9,3 +9,4 @@ if __name__ == '__main__':
   print("coucou")
   bot = HangmanBot()
   bot.run(DISCORD_TOKEN)
+  
